ac01ff3d6bb6bc38d8538cad790a86b4a1cead66
Mohamed Keita <keitamohamed12@gmail..com>
2018-05-05 12:01:39 -0400

    Added a person class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
               <p>Hey, my name is Mohamed Keita and I am a React App developer.
                   Programming is great for any one.
               </p>
+              <Person />
           </div>
       );
   }
