18b5f4e438ef399df5d391dfe82f386c4196ff4d
Keitamohamed <keitamohamed12@gamil.com>
2018-05-05 01:30:06 -0400

    Added more text.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
       return (
           <div className="App">
               <h1>Hi, I am a React App</h1>
-              <p>Hey, my name is Mohamed Keita and I am a React App developer.</p>
+              <p>Hey, my name is Mohamed Keita and I am a React App developer.
+                  Programming is great for any one.
+              </p>
           </div>
       );
   }
